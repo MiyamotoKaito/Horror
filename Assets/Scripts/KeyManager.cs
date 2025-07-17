2644af95dc7c1c9d5b387fdd62f7257594edf983
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-07-18 08:14:00 +0900

    <add>KeyManagerに鍵を使用したかの判定を追加

--- Assets/Scripts/KeyManager.cs
+++ Assets/Scripts/KeyManager.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class KeyManager : MonoBehaviour
 {
     public static KeyManager Instance { get; private set; }
-
+    //HashSetで高速検索
     private HashSet<int> _onwnedKeys = new HashSet<int>();
-
+    //イベントシステムを使って
     public System.Action<KeyBase> OnkeyObtaind;//鍵を取得
     public System.Action<KeyBase> OnkeyUsed;//鍵を仕様
-
     void Awake()
     {
         if (Instance == null)
@@ -29,9 +28,28 @@ public class KeyManager : MonoBehaviour
     /// <param name="Keytype"></param>
     public void ObtaindKey(KeyBase Keytype)
     {
-        if (_onwnedKeys.Add(KeyBase._keyID))
+        if (_onwnedKeys.Add(Keytype._keyID))
         {
-
+            OnkeyObtaind?.Invoke(Keytype);
+            Debug.Log($"鍵を取得:{Keytype._keyName}");
         }
+    }
+    /// <summary>
+    /// 鍵を持っているか確認
+    /// </summary>
+    /// <returns></returns>
+    public bool HasKey(KeyBase keytype)
+    {
+        return _onwnedKeys.Contains(keytype._keyID);
+    }
+    /// <summary>
+    /// 鍵の使用
+    /// </summary>
+    /// <param name="keytype"></param>
+    /// <returns></returns>
+    public bool IsKeyUsed(KeyBase keytype)
+    {
+        OnkeyUsed?.Invoke(keytype);
+        return true;
     }
 }
