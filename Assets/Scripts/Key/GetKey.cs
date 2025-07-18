04056cad389c547a2e19ce0d605202e5cadbfa45
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-07-18 19:27:56 +0900

    <add>鍵を入手しているときにドアを開けれるようなスクリプトを追加

---
+++ Assets/Scripts/Key/GetKey.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class GetKey : MonoBehaviour
+{
+    public static bool _getKey;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            _getKey = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
