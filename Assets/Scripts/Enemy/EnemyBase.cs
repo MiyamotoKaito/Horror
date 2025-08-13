34a4af26baddcb084e68e393b33383b366c6a9c9
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-13 19:23:12 +0900

    <delete>基底クラスの目的地を削除

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -3,7 +3,6 @@ using UnityEngine.AI;
 
 public abstract class EnemyBase : MonoBehaviour
 {
-    [SerializeField] protected GameObject destination;
     protected NavMeshAgent Agent { get; private set; }
     protected virtual void Start()
     {
