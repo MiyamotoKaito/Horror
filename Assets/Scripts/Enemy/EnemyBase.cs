36d9a292c8c14567b0b54da5f4b93de9c954f906
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-13 22:04:41 +0900

    <add>基底クラスにアニメーションを初期化して複数のクラスで同じアニメーションを使えるようにした

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -3,9 +3,11 @@ using UnityEngine.AI;
 
 public abstract class EnemyBase : MonoBehaviour
 {
+    protected Animator _anim;
     protected NavMeshAgent Agent { get; private set; }
     protected virtual void Start()
     {
+        _anim = GetComponent<Animator>();
         Agent = GetComponent<NavMeshAgent>();
         if (Agent == null)
         {
