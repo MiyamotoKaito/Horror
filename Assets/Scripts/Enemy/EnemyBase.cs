331cf914ec2b700ba870bd4a684e650405f0c194
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-16 17:36:22 +0900

    <delete>別のスクリプトで判定したいから削除

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -3,22 +3,15 @@ using UnityEngine.AI;
 
 public abstract class EnemyBase : MonoBehaviour
 {
-    [SerializeField] GameObject enemyFace;
-    [SerializeField] GameObject gameOver;
     protected Animator _anim;
     protected NavMeshAgent Agent { get; private set; }
     protected virtual void Start()
     {
-        gameOver.SetActive(false);
         _anim = GetComponent<Animator>();
         Agent = GetComponent<NavMeshAgent>();
         if (Agent == null)
         {
             Debug.LogError("NavMesh‚ª‚È‚¢");
         }
-    }
-    public void GameOver()
-    {
-        gameOver.SetActive(true);
     }
 }
