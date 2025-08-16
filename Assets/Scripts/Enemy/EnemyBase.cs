41daec7c7543f848ed697f26cc4fce9b7e3b72f2
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-16 16:25:52 +0900

    <add>エネミーの顔(顔にraycastが当たるとゲームオーバー)

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -3,6 +3,7 @@ using UnityEngine.AI;
 
 public abstract class EnemyBase : MonoBehaviour
 {
+    [SerializeField] GameObject enemyFace;
     protected Animator _anim;
     protected NavMeshAgent Agent { get; private set; }
     protected virtual void Start()
