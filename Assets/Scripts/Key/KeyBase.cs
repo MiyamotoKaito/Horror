dd0fa553cd1b54f679906086eb4e7a68cc352dfc
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-17 16:41:44 +0900

    <update>テクスチャなのに名前がオブジェクトなのは変だからKeyTextureに変更

--- Assets/Scripts/Key/KeyBase.cs
+++ Assets/Scripts/Key/KeyBase.cs
@@ -8,5 +8,5 @@ public class KeyBase : ScriptableObject
     [TextArea(5,10)]
     public string explanation;
     public KeyType keyID;
-    public RenderTexture KeyObject;
+    public RenderTexture KeyTexture;
 }
