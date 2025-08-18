32fcabcbd70449941c0bfc398cac3cb1230c8a04
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-08-18 11:16:20 +0900

    <update>砂嵐がやんだらオブジェクトと音源を止める

--- Assets/Scripts/Enemy/Eye Contact.cs
+++ Assets/Scripts/Enemy/Eye Contact.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class EyeContact : MonoBehaviour
 {
-    [SerializeField] GameObject enemyFace;
-    [SerializeField] GameObject gameOver;
-    void Start()
+    [SerializeField] private GameObject ghost;
+    [SerializeField] private GameObject gameOver;
+    private bool isTrigger;
+
+    void Awake()
     {
+        Debug.Log("awaaw");
         gameOver.SetActive(false);
     }
     public void GameOver()
     {
+        if (isTrigger == true)
+        {
+            return;
+        }
         StartCoroutine(Noise());
     }
     IEnumerator Noise()
     {
         gameOver.SetActive(true);
+        AudioManager.Instance.SEPlay("砂嵐");
+        isTrigger = true;
         yield return new WaitForSeconds(2f);
+        gameOver.SetActive(false);
+        ghost.SetActive(false);
+        AudioManager.Instance.BGMStop();
     }
 }
