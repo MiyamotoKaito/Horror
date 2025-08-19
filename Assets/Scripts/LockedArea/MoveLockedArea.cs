29d454fd68037e4c3c998b73298e3acf56f13216
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-19 19:04:54 +0900

    <add>進めない区域を移動させるスクリプト

---
+++ Assets/Scripts/LockedArea/MoveLockedArea.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLockedArea : MonoBehaviour
+{
+    [SerializeField] GameObject lockedArea;
+    [SerializeField] GameObject movePos;
+
+    public void MoveArea()
+    {
+        lockedArea.transform.position = movePos.transform.position;
+    }
+}
