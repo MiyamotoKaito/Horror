03cbc2a48073fd26915761402393b40c53d1ecdf
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-19 19:04:20 +0900

    <add>進めない区域用のスクリプト

---
+++ Assets/Scripts/LockedArea/LockedArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LockedArea : MonoBehaviour
+{
+    [SerializeField] private GameObject talkCanvas;
+    [SerializeField] private TextMeshProUGUI playerNameText;
+    [SerializeField] private TextMeshProUGUI talkText;
+    [SerializeField] private string playerName;
+    [SerializeField, TextArea(5, 10)]
+    private string talk;
+
+    public UnityEvent Action;
+
+    private void Awake()
+    {
+        talkCanvas.SetActive(false);
+        playerNameText.text = playerName;
+        talkText.text = talk;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        talkCanvas.SetActive(true);
+        Action.Invoke();
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        talkCanvas.SetActive(false);
+    }
+}
