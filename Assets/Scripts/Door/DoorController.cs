eeca9a54458be8d7212bc2bda4515eee0e70d3ba
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-08-20 00:21:28 +0900

    <feat>ドアが開かないときの音も出るようにした

--- Assets/Scripts/Door/DoorController.cs
+++ Assets/Scripts/Door/DoorController.cs
@@ -5,8 +5,9 @@ public class DoorController : MonoBehaviour
     [SerializeField] KeyBase keyType;
     private AudioSource _myAudioSource;
     private Animator _doorAnimator;
-    private bool _inTriger = false;
+    private bool _isCollect = false;
     private bool _doorOpen = false;
+    private bool _nearByDoor = false;
     private void Start()
     {
         _myAudioSource = GetComponent<AudioSource>();
@@ -21,23 +22,30 @@ public class DoorController : MonoBehaviour
     /// </summary>
     private void DoorAnimation()
     {
-        if (Input.GetKeyDown(KeyCode.F) && _inTriger == true)
-        {  
-            if (_doorOpen == false)
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (_nearByDoor && _isCollect == true)
             {
-                AudioManager.Instance.PlaySE("開ける", _myAudioSource);
-                Debug.Log("ドアを開けようとしている!!");
-                _doorAnimator.SetBool("IsOpen", true);
-                _doorAnimator.SetBool("IsClose", false);
-                _doorOpen = true;
+                if (_doorOpen == false)
+                {
+                    AudioManager.Instance.PlaySE("開ける", _myAudioSource);
+                    Debug.Log("ドアを開けようとしている!!");
+                    _doorAnimator.SetBool("IsOpen", true);
+                    _doorAnimator.SetBool("IsClose", false);
+                    _doorOpen = true;
+                }
+                else if (_doorOpen == true)
+                {
+                    AudioManager.Instance.PlaySE("閉める", _myAudioSource);
+                    Debug.Log("ドアを閉めようとしている!!");
+                    _doorAnimator.SetBool("IsClose", true);
+                    _doorAnimator.SetBool("IsOpen", false);
+                    _doorOpen = false;
+                }
             }
-            else if (_doorOpen == true)
+            else if(_nearByDoor && _isCollect == false)
             {
-                AudioManager.Instance.PlaySE("閉める", _myAudioSource);
-                Debug.Log("ドアを閉めようとしている!!");
-                _doorAnimator.SetBool("IsClose", true);
-                _doorAnimator.SetBool("IsOpen", false);
-                _doorOpen = false;
+                AudioManager.Instance.PlaySE("開かない", _myAudioSource);
             }
         }
     }
@@ -45,20 +53,22 @@ public class DoorController : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            _nearByDoor = true;
             Debug.Log("ドアにぶつかった！！");
             if (GetKey.HasKey(keyType.keyID))//特定の鍵を持っていたらドアを開けれるようにする
             {
                 Debug.Log("鍵がありましたわーい");
-                _inTriger = true;
+                _isCollect = true;
             }
         }
     }
     private void OnTriggerExit(Collider other)
     {
+        _nearByDoor = false;
         if (other.CompareTag("Player"))
         {
             Debug.Log("ドアから離れた！！");
-            _inTriger = false;
+            _isCollect = false;
         }
     }
 }
